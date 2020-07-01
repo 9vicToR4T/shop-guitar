d43add9a4db212119a361e53dee1c61f6df9d3d3
VV <victor@gmail.com>
2020-07-01 15:31:45 +0300

    create request with Fetch

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ function render() {
   productsPage.render();
 }
 
-render();
+let CATALOG = [];
+
+fetch('server/catalog.json')
+  .then((res) => res.json())
+  .then((body) => {
+    CATALOG = body;
+    render(); //renderul il facem dupa ce sa incarcat body in CATALOG, de altfel nu se vor vedea schimbarile
+  })
+  .catch((error) => {
+    console.log(error);
+  });
